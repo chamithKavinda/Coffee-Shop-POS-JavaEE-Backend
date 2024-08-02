a5a127551355bf76ab4f3be418501d6848af86d7
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-02 13:43:43 +0530

    Customer Entity done

---
+++ src/main/java/org/example/coffeeshopposjavaeebackend/entity/Customer.java
@@ -0,0 +1,15 @@
+package org.example.coffeeshopposjavaeebackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    private String custId;
+    private String custName;
+    private String custAddress;
+    private int custContact;
+}
