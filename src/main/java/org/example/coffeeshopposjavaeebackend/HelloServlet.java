1c9adad8dcd8e3f5ac1a1b911a94cb9df200f72e
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-02 10:39:05 +0530

    initial commit

---
+++ src/main/java/org/example/coffeeshopposjavaeebackend/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.example.coffeeshopposjavaeebackend;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        System.out.println("Hello");
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
