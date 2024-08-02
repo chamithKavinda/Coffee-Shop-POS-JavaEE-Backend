fac4f4b9d53a6d4407cdca727beb7c5c4c7aee38
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-02 12:31:29 +0530

    JNDI pool Implementation

--- src/main/java/org/example/coffeeshopposjavaeebackend/Controller/Customer.java
+++ src/main/java/org/example/coffeeshopposjavaeebackend/Controller/Customer.java
@@ -6,10 +6,27 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@WebServlet(urlPatterns = "/customer")
+@WebServlet(urlPatterns = "/customer",loadOnStartup = 2)
 public class Customer extends HttpServlet {
+
+    Connection connection;
+    @Override
+    public void init() throws ServletException {
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/coffeeShopPos");
+            this.connection = pool.getConnection();
+        }catch (SQLException |NamingException e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
