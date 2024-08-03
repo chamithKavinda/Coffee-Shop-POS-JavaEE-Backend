d96877783752e6352c702ad5c164575e8a32f852
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-03 11:54:35 +0530

    update BOFactory , DAOFactory

--- src/main/java/org/example/coffeeshopposjavaeebackend/bo/BOFactory.java
+++ src/main/java/org/example/coffeeshopposjavaeebackend/bo/BOFactory.java
@@ -1,6 +1,7 @@
 package org.example.coffeeshopposjavaeebackend.bo;
 
 import org.example.coffeeshopposjavaeebackend.bo.custom.impl.CustomerBOImpl;
+import org.example.coffeeshopposjavaeebackend.bo.custom.impl.ProductBOImpl;
 
 public class BOFactory {
     private static BOFactory boFactory;
@@ -12,13 +13,16 @@ public class BOFactory {
     }
 
     public enum BOTypes{
-        CUSTOMER_BO
+        CUSTOMER_BO,
+        PRODUCT_BO
     }
 
     public <T extends SuperBO> T getBO(BOTypes boTypes){
         switch (boTypes) {
             case CUSTOMER_BO:
                 return (T) new CustomerBOImpl();
+            case PRODUCT_BO:
+                return (T) new ProductBOImpl();
             default:
                 return null;
         }
--- src/main/java/org/example/coffeeshopposjavaeebackend/dao/DAOFactory.java
+++ src/main/java/org/example/coffeeshopposjavaeebackend/dao/DAOFactory.java
@@ -1,6 +1,7 @@
 package org.example.coffeeshopposjavaeebackend.dao;
 
 import org.example.coffeeshopposjavaeebackend.dao.custom.impl.CustomerDAOImpl;
+import org.example.coffeeshopposjavaeebackend.dao.custom.impl.ProductDAOImpl;
 
 public class DAOFactory {
     private static DAOFactory daoFactory;
@@ -12,13 +13,16 @@ public class DAOFactory {
     }
 
     public enum DAOTypes{
-        CUSTOMER_DAO
+        CUSTOMER_DAO,
+        PRODUCT_DAO
     }
 
     public <T extends SuperDAO> T getDAO(DAOTypes daoTypes){
         switch (daoTypes){
             case CUSTOMER_DAO:
                 return (T) new CustomerDAOImpl();
+            case PRODUCT_DAO:
+                return (T) new ProductDAOImpl();
             default:
                 return null;
         }
